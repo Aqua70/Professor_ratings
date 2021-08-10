2a911d7e2718a32b6c3acadec13e387d908fafb1
Aqua70 <ilancito7@gmail.com>
2021-08-10 13:36:19 -0400

    Now works on timetable website too

--- content.js
+++ content.js
@@ -1,25 +1,8 @@
 chrome.runtime.sendMessage({jsonData : "GET"}, function(data){
   var jsonDataTest = data.json;
-  // MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
   var json = JSON.parse(jsonDataTest.substring(5, jsonDataTest.length - 1))['response']['docs'];
   var previous = null;
-  // var observer = new MutationObserver(function mutationFound(mutations, observer) {
-  //   url =  window.location.href;
-  //   if (/https:\/\/acorn.utoronto.ca\/sws\/#\/courses\//.test(url) && url != previous) {
-  //     previous = url
-  //     acornProffesorRatings(json)
-  //   }
-  //   else if (/https:\/\/timetable.iit.artsci.utoronto.ca/.test(url)){
-  //     previous = url
-  //     timetableProffesorRatings(json)
-  //   }
-
-  // });
-  // observer.observe(document, {
-  //   subtree: true,
-  //   attributes: true
-  //   //...
-  // });
   chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
   
     var url =  window.location.href;
@@ -31,28 +14,35 @@ chrome.runtime.sendMessage({jsonData : "GET"}, function(data){
       previous = url
       timetableProffesorRatings(json)
     }
-    sendResponse({success: 2, message : "DONE..."})
+    sendResponse({success: 2, message : "Ratings have been inserted"})
     return true;
   });
 });
 
 
 function timetableProffesorRatings(teacherJSON){
-  var profs = document.querySelectorAll(".colInst")
+  var profs = document.querySelectorAll(".colInst") 
+  createRatingColoumnsTimetable();
   profs.forEach((profElement) =>{
-    profElement = profElement.childNodes[1].childNodes[1]
-    if (!profElement){
+    if (!profElement.childNodes[1].childNodes[1]){
+      createRatingBoxTimetable(profElement.parentNode, "\tN/A");
       return
     }
+    profElement = profElement.childNodes[1].childNodes[1]
     var profName = profElement.innerText
     var firstNameFirstLetter = profName.substring(profName.indexOf(', ') + 2, profName.length - 1)
     var lastName = profName.substring(0, profName.indexOf(', ')).replace(/ /, "-")
-    teacherJSON.forEach((teacher) =>{
-      if (teacher['teacherfirstname_t'].startsWith(firstNameFirstLetter) && teacher['teacherlastname_t'] === lastName){
-        
-      }
-    })
-    console.log(firstNameFirstLetter, lastName);
+    const prof_object = getProfObject(teacherJSON, firstNameFirstLetter, lastName)
+    const rating = prof_object.rating;
+    var ratingNode = profElement.parentNode.parentNode.parentNode
+    
+    if (rating != "N/A"){
+      createRatingBoxTimetable(ratingNode, `${rating.toFixed(1)}/5.0`);
+    }
+    else{
+      createRatingBoxTimetable(ratingNode, "\tN/A");
+    }
+
   })
 }
 
@@ -73,17 +63,17 @@ function acornProffesorRatings(teacherJSON){
 
         var ratingNode = prof.parentNode.parentNode
         if (rating != "N/A"){
-          createRatingBox(ratingNode, `${rating.toFixed(1)}/5.0`);
+          createRatingBoxAcorn(ratingNode, `${rating.toFixed(1)}/5.0`);
         }
         else{
-          createRatingBox(ratingNode, "\tN/A");
+          createRatingBoxAcorn(ratingNode, "\tN/A");
         }
 
       }
     }
 }
 
-function createRatingBox(parent, text){
+function createRatingBoxAcorn(parent, text){
   var rating_box = document.createElement('td');
   rating_box.innerText = text;
   rating_box.className = "instructor";
@@ -99,15 +89,40 @@ function createRatingColoumnsAcorn(){
     coloumn.class = 'rating'
     coloumn.className = "instructorDetails"
     coloumn.textContent = "Rating"
-    console.log(tr.childNodes);
     tr.insertBefore(coloumn, tr.childNodes[8])
   })
 
 }
 
+function createRatingBoxTimetable(parent, text){
+  var rating_box = document.createElement('td');
+  rating_box.innerText = text;
+  rating_box.className = "colAvail";
+  parent.insertBefore(rating_box, parent.childNodes[9]);
+}
+
+function createRatingColoumnsTimetable(){
+  var tables = document.querySelectorAll(".perMeeting")
+  tables.forEach((table) =>{
+    // console.log(table.childNodes[1].childNodes[1].className)
+    // if(table.childNodes[1].childNodes[1].className === "w100 secTut"){
+    //   return
+    // }
+    var tr = table.childNodes[1].childNodes[1].childNodes[2].childNodes[1]
+    var coloumn = document.createElement('th')
+    coloumn.scope = "col"
+    coloumn.class = 'rating'
+    coloumn.className = "headerSect colSectionAva"
+    coloumn.textContent = "Rating"
+    tr.insertBefore(coloumn, tr.childNodes[8])
+  })
+
+}
+
+
 const getProfObject = function(teacherJSON, firstName, lastName){
   for (var j = 0; j < teacherJSON.length; j++){
-    if (teacherJSON[j]['teacherfirstname_t'] == firstName && teacherJSON[j]['teacherlastname_t'] == lastName){
+    if (teacherJSON[j]['teacherfirstname_t'].startsWith(firstName) && teacherJSON[j]['teacherlastname_t'] == lastName){
       prof_object = teacherJSON[j];
       if (prof_object['averageratingscore_rf'] === undefined){
         return {rating : "N/A"}
--- popup.js
+++ popup.js
@@ -22,7 +22,7 @@ const triggerDisplay = () =>{
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {display : true}, (response) => {
             if (chrome.runtime.lastError){
-                response = {success : 0, message : "Go to a BBC video and wait until the BBC video has loaded"}
+                response = {success : 0, message : "An error has occured"}
             }
             if (messageExists)
                 document.body.removeChild(currentMessage)
